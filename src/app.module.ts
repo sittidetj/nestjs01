463e604d2dca00e3c93fd390d14c6272322c22f8
Sittidet jearsumai <sittidetj@gmail.com>
2024-10-30 10:07:51 +0700

    add graphQL to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { Api1Module } from './api1/api1.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
-    // MongooseModule.forRoot('mongodb+srv://devday:devday@developerday.qjpim.mongodb.net/sittidetje?retryWrites=true&w=majority&appName=DeveloperDay')
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     Api1Module,
   ],
